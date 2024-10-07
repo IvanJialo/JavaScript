744d0453e3723587d84949f9819b3d3b330ed9fe
unknown <ivajimalo@alu.edu.gva.es>
2024-10-07 15:32:44 +0200

    Segundo commit

--- script/main.js
+++ script/main.js
@@ -13,6 +13,11 @@ function botonPersonal() {
     alert("Aquí no se pincha, imbécil");
 }
 
+function colorRojo() {
+    let cambiarColores = document.getElementsByClassName("para")[1];
+    cambiarColores.style.color = "red";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     //console.log("DOM ha sido cargado");
 
@@ -23,6 +28,18 @@ document.addEventListener("DOMContentLoaded", () => {
     buttonNoClick.addEventListener('click', noClick);
 
     let thirdButton = document.getElementById("third");
-
     thirdButton.addEventListener('click', botonPersonal);
+
+    let misClases = document.getElementsByClassName("para");
+    console.log(misClases);
+
+    misClases[2].classList.add('negrita');
+    misClases[4].classList.remove('para');
+
+    for (let index = 0; index < misClases.length; index++) {
+        misClases[index].classList.add("hola");
+    }
+
+    let cambiarColor = document.getElementsByClassName("para");
+    cambiarColor.addEventListener('click', colorRojo);
 })
